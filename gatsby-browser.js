7f79bd57cbf2643296c121b87ccfd5fba984ad38
Heonyoung Kim <altenull@gmail.com>
2020-10-26 00:33:23 +0900

    style: add GlobalStyle to root element

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,5 +3,21 @@
  *
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
-
 // You can delete this file if you're not using it
+const React = require('react');
+const { createGlobalStyle } = require('styled-components');
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`;
+
+exports.wrapRootElement = ({ element }) => {
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      {element}
+    </React.Fragment>
+  );
+};
