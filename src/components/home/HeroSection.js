0777a3fe09c0580fd3900c0f1d6936cb7e81830a
Heonyoung Kim <altenull@gmail.com>
2020-10-27 14:52:54 +0900

    style: update HeroSection styles

--- src/components/home/HeroSection.js
+++ src/components/home/HeroSection.js
@@ -4,42 +4,49 @@ import HeroImage from './HeroImage';
 
 const StdHeroSection = styled.section`
   position: relative;
-  height: 80%;
+  height: 448px;
   background-color: ${(props) => props.theme.colors.gray0};
 `;
 
+const StdContentHolder = styled.div`
+  display: inline-block;
+  padding-left: 64px;
+  padding-top: 64px;
+`;
+
 const StdTitle = styled.h1`
-  margin: 0;
-  font-size: 64px;
-  transform: translate(220px, 140px);
+  margin: 0 0 16px;
+  font-size: 52px;
   color: ${(props) => props.theme.colors.gray4};
 `;
 
 const StdDescription = styled.p`
   margin: 0;
-  font-size: 21px;
+  font-size: 18px;
   font-weight: ${(props) => props.theme.fontWeights.light};
-  max-width: 600px;
+  max-width: 680px;
   color: ${(props) => props.theme.colors.gray2};
-  transform: translate(220px, 180px);
 `;
 
 const StdHeroImage = styled(HeroImage)`
   position: absolute;
-  right: 128px;
-  top: 320px;
-  width: 440px;
+  right: 32px;
+  bottom: 48px;
+  width: 400px;
   height: auto;
 `;
 
 const HeroSection = () => {
   return (
     <StdHeroSection>
-      <StdTitle>Design System Starter</StdTitle>
-      <StdDescription>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-        standard dummy text ever since the 1500s.
-      </StdDescription>
+      <StdContentHolder>
+        <StdTitle>Design System Starter</StdTitle>
+        <StdDescription>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+          standard dummy text ever since the 1500s.
+        </StdDescription>
+      </StdContentHolder>
+
       <StdHeroImage />
     </StdHeroSection>
   );
