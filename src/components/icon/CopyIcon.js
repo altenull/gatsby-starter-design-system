c5ebe6bb1b65dfbdac9243ea662c7270f8cb2cb6
Heonyoung Kim <altenull@gmail.com>
2020-10-27 23:30:48 +0900

    refactor: add size prop to every icon component

--- src/components/icon/CopyIcon.js
+++ src/components/icon/CopyIcon.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -7,13 +8,21 @@ const StdSvg = styled.svg`
   fill: ${(props) => props.theme.colors.white};
 `;
 
-const CopyIcon = ({ ...props }) => {
+const CopyIcon = ({ size, ...props }) => {
   return (
-    <StdSvg xmlns='http://www.w3.org/2000/svg' width='24' heigth='24' viewBox='0 0 488.3 488.3' {...props}>
+    <StdSvg xmlns='http://www.w3.org/2000/svg' width={size} heigth={size} viewBox='0 0 488.3 488.3' {...props}>
       <path d='M314.3 85.4h-227c-21.3 0-38.6 17.3-38.6 38.6v325.7c0 21.3 17.3 38.6 38.6 38.6h227c21.3 0 38.6-17.3 38.6-38.6V124C352.8 102.7 335.5 85.4 314.3 85.4zM325.8 449.6c0 6.4-5.2 11.6-11.6 11.6h-227c-6.4 0-11.6-5.2-11.6-11.6V124c0-6.4 5.2-11.6 11.6-11.6h227c6.4 0 11.6 5.2 11.6 11.6V449.6z' />
       <path d='M401.1 0h-227c-21.3 0-38.6 17.3-38.6 38.6 0 7.5 6 13.5 13.5 13.5s13.5-6 13.5-13.5c0-6.4 5.2-11.6 11.6-11.6h227c6.4 0 11.6 5.2 11.6 11.6v325.7c0 6.4-5.2 11.6-11.6 11.6 -7.5 0-13.5 6-13.5 13.5s6 13.5 13.5 13.5c21.3 0 38.6-17.3 38.6-38.6V38.6C439.7 17.3 422.4 0 401.1 0z' />
     </StdSvg>
   );
+};
+
+CopyIcon.propTypes = {
+  size: PropTypes.number,
+};
+
+CopyIcon.defaultProps = {
+  size: 16,
 };
 
 export default CopyIcon;
--- src/components/icon/SearchIcon.js
+++ src/components/icon/SearchIcon.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -5,15 +6,23 @@ const StdSvg = styled.svg`
   display: block;
 `;
 
-const SearchIcon = () => {
+const SearchIcon = ({ size, ...props }) => {
   return (
-    <StdSvg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 16 16'>
+    <StdSvg xmlns='http://www.w3.org/2000/svg' width={size} height={size} viewBox='0 0 16 16' {...props}>
       <path
         d='M15.7 14.3l-4.2-4.2c-0.2-0.2-0.5-0.3-0.8-0.3 0.8-1 1.3-2.4 1.3-3.8 0-3.3-2.7-6-6-6s-6 2.7-6 6 2.7 6 6 6c1.4 0 2.8-0.5 3.8-1.4 0 0.3 0 0.6 0.3 0.8l4.2 4.2c0.2 0.2 0.5 0.3 0.7 0.3s0.5-0.1 0.7-0.3c0.4-0.3 0.4-0.9 0-1.3zM6 10.5c-2.5 0-4.5-2-4.5-4.5s2-4.5 4.5-4.5 4.5 2 4.5 4.5-2 4.5-4.5 4.5z'
         fill='#868e96'
       />
     </StdSvg>
   );
+};
+
+SearchIcon.propTypes = {
+  size: PropTypes.number,
+};
+
+SearchIcon.defaultProps = {
+  size: 16,
 };
 
 export default SearchIcon;
