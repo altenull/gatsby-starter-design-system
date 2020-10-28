02ada7d96a860f076837e1f438a0c24461581c3c
Heonyoung Kim <altenull@gmail.com>
2020-10-28 10:29:59 +0900

    feat: add theme util

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,6 +8,8 @@
 const React = require('react');
 const { createGlobalStyle, ThemeProvider } = require('styled-components');
 
+const { defaultTheme } = require('./src/theme/theme.util');
+
 require('prismjs/themes/prism-tomorrow.css');
 
 const GlobalStyle = createGlobalStyle`
@@ -29,37 +31,8 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 exports.wrapRootElement = ({ element }) => {
-  const theme = {
-    sharedSizes: {
-      headerHeight: '64px',
-      sidebarWidth: '240px',
-    },
-    colors: {
-      gray0: '#e9ecef',
-      gray1: '#ced4da',
-      gray2: '#868e96',
-      gray3: '#495057',
-      gray4: '#343a40',
-      primary: '#ae3ec9',
-    },
-    zIndexes: {
-      sidebar: 900,
-      header: 1000,
-    },
-    fontWeights: {
-      light: 300,
-      regular: 400,
-      semiBold: 600,
-    },
-    duration: {
-      fast: '100ms',
-      normal: '200ms',
-      slow: '300ms',
-    },
-  };
-
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       {element}
     </ThemeProvider>
---
+++ src/theme/theme.util.js
@@ -0,0 +1,31 @@
+import { createMediaQueries } from './media-query.util';
+
+export const defaultTheme = {
+  mediaQueries: createMediaQueries(),
+  sharedSizes: {
+    headerHeight: '64px',
+    sidebarWidth: '240px',
+  },
+  colors: {
+    gray0: '#e9ecef',
+    gray1: '#ced4da',
+    gray2: '#868e96',
+    gray3: '#495057',
+    gray4: '#343a40',
+    primary: '#ae3ec9',
+  },
+  zIndexes: {
+    sidebar: 900,
+    header: 1000,
+  },
+  fontWeights: {
+    light: 300,
+    regular: 400,
+    semiBold: 600,
+  },
+  duration: {
+    fast: '100ms',
+    normal: '200ms',
+    slow: '300ms',
+  },
+};
