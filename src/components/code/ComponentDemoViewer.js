d58ea415f605ea2525533a253a8c9559725c98ff
Heonyoung Kim <altenull@gmail.com>
2020-10-29 23:10:59 +0900

    refactor: separate background styles of demo pane to css variable to make it users to change easy

--- src/components/code/ComponentDemoViewer.js
+++ src/components/code/ComponentDemoViewer.js
@@ -1,16 +1,22 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { CodeHighlighterContainer } from '../../containers/code';
+
+// You can change background styles of demo pane here.
+const demoPaneBackgroundCss = css`
+  background-color: #ffffff;
+  background: linear-gradient(90deg, #ffffff 10px, transparent 1%) center,
+    linear-gradient(#ffffff 10px, transparent 1%) center, ${(props) => props.theme.colors.gray2};
+  background-size: 11px 11px;
+`;
 
 const StdDemoPane = styled.div`
   margin-bottom: -8px;
   padding: 40px 24px;
   border: 1px solid ${(props) => props.theme.colors.gray0};
-  background-color: #ffffff;
-  background: linear-gradient(90deg, #ffffff 10px, transparent 1%) center,
-    linear-gradient(#ffffff 10px, transparent 1%) center, ${(props) => props.theme.colors.gray2};
-  background-size: 11px 11px;
+
+  ${demoPaneBackgroundCss}
 `;
 
 const ComponentDemoViewer = ({ demo, codeInHtml, codeInRawMarkdownBody, ...props }) => (
